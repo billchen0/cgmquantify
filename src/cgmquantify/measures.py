b2c6bc5a3bc2a654a1bd973ba6d6107c98d7aa05
jvaldovin <67069015+jvaldovin@users.noreply.github.com>
2025-03-04 14:00:21 -0800

    Created CV function.

--- src/cgmquantify/measures.py
+++ src/cgmquantify/measures.py
@@ -2,12 +2,13 @@ import pandas as pd
 from scipy import stats
 
 
-def mad_glu(df: pd.DataFrame, constant) -> pd.DataFrame:
+def mad_glu(df: pd.DataFrame, scaling: float) -> pd.DataFrame:
     """
     Compute the median absolute deviation (MAD) of glucose values for each subject.
 
     Args:
         df (pd.DataFrame): DataFrame with 'id' and 'gl' columns (subject ID and glucose values).
+        scaling (float): Scaling factor for MAD calculation.
 
     Returns:
         pd.DataFrame: A DataFrame with one row per subject.
@@ -15,11 +16,11 @@ def mad_glu(df: pd.DataFrame, constant) -> pd.DataFrame:
     if not {"id", "gl"}.issubset(df.columns):
         raise ValueError("DataFrame must contain 'id' and 'gl' columns.")
 
-    # Compute the percentage of values above each threshold for each subject
+    # Compute the median absolute deviation with scaling for each subject.
     result = (
         df.groupby("id")["gl"]
         .agg(
-            lambda x: {f"MAD": stats.median_abs_deviation(x, scale = constant)}
+            lambda x: {f"MAD": stats.median_abs_deviation(x, scale = scaling)}
         )
         .apply(pd.Series)
         .reset_index()
