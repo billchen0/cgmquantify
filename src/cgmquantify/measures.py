56dc578b5c44e717543d3ea5554953f5e3ad7d31
jvaldovin <67069015+jvaldovin@users.noreply.github.com>
2025-03-21 10:19:15 -0700

    Fixed an error in using nanquantile.

--- src/cgmquantify/measures.py
+++ src/cgmquantify/measures.py
@@ -159,26 +159,28 @@ def range_glu(df: pd.DataFrame) -> pd.DataFrame:
 
 def quantile_glu(df: pd.DataFrame, quantiles=[0, 25, 50, 75, 100]) -> pd.DataFrame:
     """
-    Compute the coefficient of variation (SD/mean) for each subject.
+    Compute the quantiles for each subject.
 
     Args:
         df (pd.DataFrame): DataFrame with 'id' and 'gl' columns (subject ID and glucose values).
 
     Returns:
-        pd.DataFrame: A DataFrame with one row per subject, each threshold as a separate column.
+        pd.DataFrame: A DataFrame with one row per subject, each a quantile as a separate column.
     """
     if not {"id", "gl"}.issubset(df.columns):
         raise ValueError("DataFrame must contain 'id' and 'gl' columns.")
 
-    # Compute the percentage of values above each threshold for each subject
+    # Compute the quantile values for each subject
     result = (
         df.groupby("id")["gl"]
         .agg(
-            lambda x: {f"CV": (np.std(x)/np.nanmean(x))*100}
+            lambda x: {
+                f"X{t}": np.nanquantile(x,t/100) for t in quantiles
+            }
         )
         .apply(pd.Series)
         .reset_index()
     )
-
+    print(result)
     return result  # Correctly formatted wide-format DataFrame
 
