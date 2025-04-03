9bef15e52e2ac86841792b1bc5164eec67d92414
jvaldovin <67069015+jvaldovin@users.noreply.github.com>
2025-04-03 11:10:09 -0700

    Added J_index measure to measures document.

--- src/cgmquantify/measures.py
+++ src/cgmquantify/measures.py
@@ -184,3 +184,31 @@ def quantile_glu(df: pd.DataFrame, quantiles=[0, 25, 50, 75, 100]) -> pd.DataFra
     print(result)
     return result  # Correctly formatted wide-format DataFrame
 
+def j_index(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Compute the j_index for each subject.
+    The equation and rationale for j_index is found here:
+    Wojcicki (1995), "J_index" A New Proposition of the Assessment of Current Glucose Control
+    in Diabetic Patients.
+
+    Args:
+        df (pd.DataFrame): DataFrame with 'id' and 'gl' columns (subject ID and glucose values).
+
+    Returns:
+        pd.DataFrame: A DataFrame with one row per subject.
+    """
+    if not {"id", "gl"}.issubset(df.columns):
+        raise ValueError("DataFrame must contain 'id' and 'gl' columns.")
+
+    # Compute the percentage of values above each threshold for each subject
+    result = (
+        df.groupby("id")["gl"]
+        .agg(
+            lambda x: {f"J_index": 0.001*(np.nanmean(x)+np.std(x))**2}
+        )
+        .apply(pd.Series)
+        .reset_index()
+    )
+
+    return result  # Correctly formatted wide-format DataFrame
+
